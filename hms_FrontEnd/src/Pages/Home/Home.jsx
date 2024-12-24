0cf71d5de607ccbc7e68714b3ecc8e3636fa7a36
SewwandiPeiris <peirissewwandi@gmail.com>
2024-12-25 00:37:10 +0530

    Home Page Background

--- hms_FrontEnd/src/Pages/Home/Home.jsx
+++ hms_FrontEnd/src/Pages/Home/Home.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../../Components/Header';
 import Footer from '../../Components/Footer';
+import buildingImage from '../../assets/image2.jpg';
+import './Home.css'
 
 const Home= () => {
   return (
     <>
      <Header/>
-     <div><br/><br></br></div>
+     <div className="home-container">
+      <div className="background-image">
+        <img src={buildingImage} alt="Hostel Building" className="img-fluid" />
+      </div>
+      <div className="content-box p-4 rounded shadow">
+        <h1 className="custom-color">HMS</h1>
+        <h2 className="custom-color1">Hostel Management System</h2>
+        <h3 className="custom-color2 ">Welcome to Our Hostel!</h3>
+        <p>
+          Experience comfort and convenience in a safe and welcoming environment. 
+          Our hostel offers modern facilities, spacious rooms, and a vibrant community, 
+          designed to make you feel at home.Whether you're here for studies or work, 
+          we ensure a peaceful stay with all the amenities you need.
+        </p>
+      </div>
+     </div>
      <Footer/>
     </>
   );
