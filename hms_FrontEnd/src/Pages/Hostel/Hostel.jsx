66f15c5a07f93f6475e95670d8b8a3d8118092a4
bingumi-123 <bingumichamudika9@gmail.com>
2024-12-24 21:01:22 +0530

    Hostel.page

--- hms_FrontEnd/src/Pages/Hostel/Hostel.jsx
+++ hms_FrontEnd/src/Pages/Hostel/Hostel.jsx
@@ -2,12 +2,40 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SideBar from '../../Components/SideBar';
 import Tool from '../../Components/Tool';
-
-const Home= () => {
+import "./Hostel.css";
+const Home = () => {
   return (
     <>
       <SideBar />
-      <Tool/>
+      <Tool />
+      <div className="hostels-container">
+        <h1 className="hostels-title">Hostels</h1>
+        <button className="add-hostel-button">Add Hostel</button>
+        <table className="hostels-table">
+          <thead>
+            <tr>
+              <th>Hostel ID</th>
+              <th>Hostel Category</th>
+              <th>Hostel Name</th>
+              <th>Location</th>
+              <th>Total Rooms</th>
+              <th>Total Capacity</th>
+              <th>Filled Capacity</th>
+              <th>Available Capacity</th>
+              <th>Total Beds</th>
+              <th>Total Tables</th>
+              <th>Total Chairs</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Rows can be dynamically rendered here */}
+            <tr>
+              <td colSpan="12" className="empty-row">No data available</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
