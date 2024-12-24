e3e20d9c573285da8bfcb79d6ad46d4bb7cf7ada
bingumi-123 <bingumichamudika9@gmail.com>
2024-12-24 20:46:30 +0530

    change_Tool

--- hms_FrontEnd/src/Components/Tool.jsx
+++ hms_FrontEnd/src/Components/Tool.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-
 import './Tool.css';
 
 const Header = () => {
   return (
     <Container>
-    <Navbar expand="lg" className="custom-navbar">
-      <Container>
-        <Navbar.Brand href="#home">University of Kelaniya</Navbar.Brand>
-        
-      </Container>
-    </Navbar></Container>
+      <Navbar expand="lg" className="custom-navbar">
+        <Container>
+          <Navbar.Brand href="#home" className="navbar-brand">
+            University of Kelaniya
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </Container>
   );
 };
 
