e5adb8b192dc4bf92b6c7c386e103e666a18b3e6
VinulVinsura <155805055+VinulVinsura@users.noreply.github.com>
2025-03-10 16:54:15 +0530

    room and eligible student delete method done

--- hms_FrontEnd/src/Service/adminService.js
+++ hms_FrontEnd/src/Service/adminService.js
@@ -29,3 +29,66 @@ export const addRoom=(token,room)=>axios.post("http://localhost:8081/api/room/ad
         "Content-Type": "application/json",
     },
 });
+
+export const getAllProspectiveStudentByFilter=(token,param)=>axios.get("http://localhost:8081/api/user/get-all-bySalaryOrDistanceOrGender",{
+    headers: {
+        Authorization: `Bearer ${token}`, // Attach token
+        "Content-Type": "application/json",
+
+    },
+    params:param
+});
+
+
+export const getAllEligibleStudent=(token)=>axios.get("http://localhost:8081/api/user/get-all-eligible-student",{
+    headers: {
+        Authorization: `Bearer ${token}`, // Attach token
+        "Content-Type": "application/json",
+    },
+});
+
+export const getRoomByHostelId=(id,token)=>axios.get("http://localhost:8081/api/room/get-room-hostelId/"+id, {
+    headers: {
+        Authorization: `Bearer ${token}`, // Attach token
+        "Content-Type": "application/json",
+    },
+});
+
+
+export const addEligibleStudent=(token,student)=>axios.post("http://localhost:8081/api/user/save-user",student,{
+    headers: {
+        Authorization: `Bearer ${token}`, // Attach token
+        "Content-Type": "application/json",
+    },
+});
+
+
+export const updateHostelAndRoomCapacity=(token,hostelId,commonDto)=>axios.put("http://localhost:8081/api/hostel/update-hostel-capacity/"+hostelId,commonDto,{
+    headers: {
+        Authorization: `Bearer ${token}`, // Attach token
+        "Content-Type": "application/json",
+    },
+});
+
+export const deleteEligibleStudent=(token,id)=>axios.delete("http://localhost:8081/api/user/delete-eligible-student/"+id,{
+    headers: {
+        Authorization: `Bearer ${token}`, // Attach token
+        "Content-Type": "application/json",
+    },
+});
+
+export const deleteRoomById=(token,id)=>axios.delete("http://localhost:8081/api/room/delete-room/"+id,{
+    headers: {
+        Authorization: `Bearer ${token}`, // Attach token
+        "Content-Type": "application/json",
+    },
+});
+
+
+
+
+
+
+
+
+
